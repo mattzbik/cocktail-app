e54c7e458ca9fa5b40606b4fcb6fc301538139f9
Matt <matt.j.zbik@gmail.com>
2023-05-05 13:54:01 -0600

    - Add eslint ignore back.

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,8 +10,8 @@ const main = () => {
   app.use(cors());
 
   app.listen(process.env.PORT, () => {
+    // eslint-disable-next-line no-console
     console.log(`Server running on port ${process.env.PORT}.`);
-    // test
   });
 };
 
